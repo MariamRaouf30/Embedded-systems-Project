0e2457396459a70c9809bfa9fc19bf5f22ea4a36
George-essam <georgeessam00@gmail.com>
2022-05-21 23:24:30 +0200

    switches

---
+++ switches.h
@@ -0,0 +1,9 @@
+void SW1_Init(void);
+void SW2_Init(void);
+void SW3_Init(void);
+unsigned sw_Input(void);
+unsigned sw3_Input(void);
+void Buzzer_VInit (void) ; 
+void Buzzer_ON (void) ; 
+void Buzzer_OFF (void) ;
+void Buzzer_TOGGLE (void);
